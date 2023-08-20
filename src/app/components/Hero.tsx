c8649d38c6a54453f21a10817abe4070cad6aba3
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 12:25:22 +0100

    Add responsiveness breakpoints for /components/Hero.tsx main div

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,7 @@ import styles from "./styles/Hero.module.css"
 
 const Hero = () => {
   return (
-    <div id = "hero" className={`${styles.main}`}>
+    <div id = "hero" className={`min-h-[800px] sm:min-h-[1000px] md:min-h-[1200px] lg:min-h-[1400px] xl:min-h-[1600px] ${styles.main}`}>
         Hero
     </div>
   )
