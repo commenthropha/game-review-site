77634c4b766e4d9953a2d1174be9ab42a57a2892
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 18:03:29 +0100

    Change /components/About.tsx h3 fonts to Inconsolata

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -25,8 +25,8 @@ const About = () => {
       className={`mt-20 border-8 border-double rounded-md border-red-900 border-solid m-2 p-8 sm:m-20 md:m-24 flex flex-col`}
     >
       <div className="m-3 flex flex-col md:flex-row justify-between md:items-center">
-        <h3 className="text-red-800 min-w-[8rem] font-bold text-3xl">What Is Our Aim?</h3>
-        <p className="mt-4 md:mt-0 max-w-[90rem] pl-0 md:pl-6">
+        <h3 className="tracking-tighter font-inconsolata text-red-800 min-w-[8rem] font-bold text-3xl">What Is Our Aim?</h3>
+        <p className="tracking-tighter font-inconsolatamt-4 md:mt-0 max-w-[90rem] pl-0 md:pl-6">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec nulla
           vitae massa molestie malesuada. Suspendisse potenti. In fringilla dui
           gravida eros eleifend, a blandit nibh dapibus. Aenean dapibus
@@ -39,12 +39,12 @@ const About = () => {
       </div>
       <div className="my-10 flex flex-col md:flex-row justify-between md:items-center">
         <h2 className="font-extrabold text-5xl md:ml-4">Level Up Your Game</h2>
-        <p className="text-red-700 mt-4 md:mt-0">See Our Reviews</p>
+        <p className="text-red-700 mt-4 md:mt-0 tracking-tighter font-inconsolata">See Our Reviews</p>
       </div>
       <div className="flex flex-col justify-between lg:flex-row">
         {data.map((item, index) => (
           <div key={index} className="m-4 md:m-8">
-            <h3 className="text-red-800 font-bold text-3xl">{item.title}</h3>
+            <h3 className="tracking-tighter font-inconsolata text-red-800 font-bold text-3xl">{item.title}</h3>
             <hr className="mt-1 border-red-800"/>
             <p className="mt-3 " >{item.content}</p>
           </div>
