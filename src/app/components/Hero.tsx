83f09c5cb1c1099490768ae3d7ce44d6091868c1
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 17:33:07 +0100

    Remove shadow variable from /components/Hero.tsx main div

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -10,14 +10,14 @@ const Hero = () => {
       >
         <div className="pl-[50px] sm:pl-[100px] pt-[150px] md:pt-[200px] md:pl-[150px] lg:pt-[225px] lg:pl-[200px] xl:pl-[300px]">
           <h1
-            className={`font-extrabold text-5xl sm:text-6xl md:text-7xl xl:text-8xl ${styles.shadow}`}
+            className={`font-extrabold text-5xl sm:text-6xl md:text-7xl xl:text-8xl`}
           >
             Your Games,
             <br /> Reviewed
           </h1>
         </div>
       </div>
-      <p className="text-slate-600 italic text-center text-bottom mt-4 mx-12 text-xs md:text-sm">
+      <p className="text-red-900 italic text-center text-bottom mt-4 mx-12 text-xs md:text-sm">
         Red Dead Redemption 2
       </p>
     </div>
