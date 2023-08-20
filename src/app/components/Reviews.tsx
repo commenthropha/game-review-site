442d1b0ec17089ba8f1d82299896f2f41cf0c180
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 18:03:59 +0100

    Add h1 element to /components/Reviews.tsx

--- src/app/components/Reviews.tsx
+++ src/app/components/Reviews.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+    <div className='bg-[#ffeec4] text-[#171520]'>
+        <h1 className='tracking-tighter font-inconsolata p-32 text-6xl text-center font-extrabold'>Take A Look At Some Of Our Reviews</h1>
+    </div>
   )
 }
 
