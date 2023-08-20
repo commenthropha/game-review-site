d7d2d852a9a8930963ef0aa894ca963e07d7cfe2
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 21:09:53 +0100

    Debugging Vercel deployment issue

---
+++ src/app/(queries)/getRecentPosts.ts
@@ -0,0 +1,30 @@
+const getRecentPosts = async () => {
+  // @ts-ignore
+  const { data } = await fetch(process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+    query getRecentPosts {
+      posts(orderBy: publishedAt_ASC, last: 4) {
+        title
+        id
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `,
+    }),
+    next: { revalidate: 10 },
+  }).then((res) => res.json());
+
+  let posts: Post[] = data?.posts
+  return posts;  
+};
+
+export default getRecentPosts;
