3fd271888dd228ba1afee1d03e9161f18f061759
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 19:11:48 +0100

    Write /(queries)/getRecentPosts.ts query

---
+++ src/app/(queries)/getRecentPosts.ts
@@ -0,0 +1,23 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlAPI: string = process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT ? process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT : "";
+
+const getRecentPosts = async () => {
+  const query = gql`
+    query getRecentPosts {
+      posts(orderBy: publishedAt_ASC, last: 4) {
+        title
+        id
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const { posts }: { posts: Post[] } = await request(graphqlAPI, query);
+
+  return posts;
+};
