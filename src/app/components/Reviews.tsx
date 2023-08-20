fbf279cfa2b6b07d652e26670d1865630cd66f73
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 20:19:40 +0100

    Update /components/Reviews.tsx stylisation

--- src/app/components/Reviews.tsx
+++ src/app/components/Reviews.tsx
@@ -1,11 +1,21 @@
-import { request, gql } from 'graphql-request';
+import getRecentPosts from '../(queries)/getRecentPosts';
+import Card from './Card';
 
-const Reviews = () => {
+const Reviews = async () => {
+  const posts = await getRecentPosts();
+
   return (
     <div className='bg-[#ffeec4] text-[#171520]'>
-        <h1 className='tracking-tighter font-inconsolata p-32 text-6xl text-center font-extrabold'>Take A Look At Some Of Our Reviews</h1>
+      <h1 className='p-32 text-6xl text-center font-extrabold'>
+        Take A Look At Some Of Our <span style ={{wordSpacing: "-20px"}} className='tracking-tighter font-inconsolata text-red-800'>Recent Reviews</span>
+      </h1>
+      <div className='flex flex-col xl:flex-row'>
+        {posts?.map((post) => (
+          <Card key={post.id} post = {post}/>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
