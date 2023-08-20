17443e9654d54e892bc01f74417a4e6dc4221077
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 22:11:42 +0100

    Update /components/Card.tsx colors

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -5,7 +5,7 @@ import moment from "moment";
 const Card = ({ post, reverse }: { post: Post, reverse: boolean }) => {
   
   return (    
-    <div className="bg-red-800 border-2 border-solid border-[#fbe09f] xl:w-full m-2 rounded-lg">
+    <div className="bg-[#FFFAEF] border-2 border-solid border-[#fbe09f] xl:w-full m-2 rounded-lg">
       <Link href={`/`}>
         <div className= {`flex ${reverse? "flex-row-reverse" : "flex-row"} xl:${reverse ? "flex-col-reverse" : "flex-col"} justify-between h-full`}>
         <div className="relative w-[8rem] sm:w-[12rem] md:w-[20rem] lg:w-[24rem] h-auto xl:w-auto xl:h-[20rem]">
@@ -19,11 +19,11 @@ const Card = ({ post, reverse }: { post: Post, reverse: boolean }) => {
         </div>
           <div className={`${reverse ? "text-left" : "text-right"} font-bold m-6 xl:m-4 xl:text-left flex flex-col justify-between`}>
             <div>
-              <p className="text-[#FEF5DF] tracking-tighter font-inconsolata text-sm">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
-              <h3 className="text-[#FEF5DF]">{post.title}</h3>
+            <p className="text-red-800 tracking-tighter font-inconsolata text-sm">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
+              <h3 className="text-[#171520]">{post.title}</h3>
             </div>
             <div className="mt-24">
-              <p className="text-red-300 tracking-tighter font-inconsolata text-sm">Read Full Article</p>
+              <p className="text-stone-500 tracking-tighter font-inconsolata text-sm">Read Full Article</p>
             </div>
           </div>
         </div>
