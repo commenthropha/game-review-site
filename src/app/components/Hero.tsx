680c43ce7df95b3b652d229973e10acf61afabd8
Anees Haroon <aharoon1007@gmail.com>
2023-08-20 14:49:10 +0100

    Update /components/Hero.tsx with responsiveness and scaling

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import styles from "./styles/Hero.module.css"
+import React from "react";
+import styles from "./styles/Hero.module.css";
 
 const Hero = () => {
   return (
-    <div id = "hero" className={`min-h-[800px] sm:min-h-[1000px] md:min-h-[1200px] lg:min-h-[1400px] xl:min-h-[1600px] ${styles.main}`}>
-        Hero
+    <div
+      id="hero"
+      className={`min-h-[800px] md:min-h-[900px] xl:min-h-[1000px] ${styles.main}`}
+    >
+      <div className="pl-[50px] sm:pl-[100px] pt-[150px] md:pt-[200px] md:pl-[150px] lg:pt-[225px] lg:pl-[200px] xl:pl-[300px]">
+        <h1
+          className={`font-extrabold text-5xl sm:text-6xl md:text-7xl xl:text-8xl ${styles.shadow}`}
+        >
+          Your Games,
+          <br /> Reviewed
+        </h1>
+      </div>
+      <div className="text-slate-500 italic text-center text-bottom pt-72 xl:pt-[22rem] mx-12">
+        Taken from Red Dead Redemption 2
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
