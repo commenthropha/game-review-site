f2565de1ea9e18cbbabe259e9230cce7310af366
Anees Haroon <aharoon1007@gmail.com>
2023-08-22 18:52:47 +0100

    Import getPosts query into /reviews/page.tsx

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,4 +1,8 @@
-const Reviews = () => {
+import {getPosts} from "../(queries)"
+
+const Reviews = async () => {
+  const posts = await getPosts();
+
   return (
     <h1 className="p-6 md:p-16 lg:p-24 font-extrabold text-5xl md:text-6xl lg:text-7xl">
       Reviews
