fe26984380cf95c0d3853fa190b036d55650081c
Anees Haroon <aharoon1007@gmail.com>
2023-08-22 12:25:29 +0100

    Remove Navbar from /components/index.js named exports

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,5 +1,4 @@
 export { default as Hero } from "./Hero";
-export { default as Navbar } from "./Navbar";
 export { default as About } from "./About"
 export { default as Reviews } from "./Reviews"
 
