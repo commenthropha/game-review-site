25438f175a7e9bc2a0c1dc51412ed6e891110130
Anees Haroon <aharoon1007@gmail.com>
2023-08-22 21:38:55 +0100

    Update /reviews/components/Card.tsx styling

--- src/app/reviews/components/Card.tsx
+++ src/app/reviews/components/Card.tsx
@@ -4,9 +4,9 @@ import moment from "moment";
 
 const Card = ({ post }: { post: Post }) => {
   return (
-    <div className="bg-honey my-3 md:m-2 lg:my-4 rounded-xl">
+    <div className="bg-red-900 my-3 md:m-2 lg:my-4 rounded-xl">
       <Link href={`/`}>
-        <div className= {`flex justify-between h-full`}>
+        <div className= {`flex justify-start h-full`}>
         <div className="relative w-[8rem] sm:w-[12rem] md:w-[18rem] 2xl:h-[15rem]">
           <Image
               alt={post.title}
@@ -16,13 +16,13 @@ const Card = ({ post }: { post: Post }) => {
               objectFit="cover" // Make the image fill the container
             />
         </div>
-          <div className= "text-right font-bold m-6 2xl:m-8 flex flex-col justify-between">
+          <div className= "text-left font-bold m-6 2xl:m-8 flex flex-col justify-between">
             <div>
-            <p className="text-red-800 tracking-tighter font-inconsolata text-sm">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
-              <h3 className="text-[#171520]">{post.title}</h3>
+            <p className="tracking-tighter font-inconsolata text-xs md:text-sm xl:text-md">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
+              <h3 className="text-md md:text-lg xl:text-2xl">{post.title}</h3>
             </div>
             <div className="mt-12">
-              <p className="text-stone-500 tracking-tighter font-inconsolata text-sm">Read Full Article</p>
+              <p className="text-red-400 tracking-tighter font-inconsolata text-xs md:text-sm">Read Full Article</p>
             </div>
           </div>
         </div>
