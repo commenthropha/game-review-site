7a9f09a4c024347159dc693ad420e0b1ea43d74f
Anees Haroon <aharoon1007@gmail.com>
2023-08-22 21:39:03 +0100

    Update /reviews/components/FeatureCard.tsx styling

--- src/app/reviews/components/FeatureCard.tsx
+++ src/app/reviews/components/FeatureCard.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 import moment from "moment";
 
 const FeatureCard = ({ post }: { post: Post }) => {
+  console.log(post.title);
   return (
-    <div className="bg-red-900 md:ml-2 md:mb-6 rounded-md">
+    <div className="bg-honey md:ml-2 md:mb-6 rounded-md">
       <Link href={`/`}>
         <div className= "flex justify-between h-full flex-row-reverse">
         <div className="relative w-[10rem] sm:w-[14rem] md:w-[22rem] lg:w-[26rem] 2xl:h-[20rem]">
@@ -18,11 +19,12 @@ const FeatureCard = ({ post }: { post: Post }) => {
         </div>
           <div className= "text-left font-bold m-6 2xl:m-10 flex flex-col justify-between">
             <div>
-            <p className="text-honey tracking-tighter font-inconsolata text-sm">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
-              <h3 className="text-honey">{post.title}</h3>
+            <p className="text-red-800 tracking-tighter font-inconsolata text-sm md:text-md xl:text-lg">{moment(post.createdAt).format("MMM DD, YYYY")}</p>
+            <h3 className="text-xl text-regal md:text-2xl xl:text-4xl">{post.title}</h3>
+            <p className="text-regal tracking-tighter font-inconsolata text-md md:text-lg xl:text-2xl mt-4">{post.subheading}</p>
             </div>
             <div className="mt-24">
-              <p className="text-honey tracking-tighter font-inconsolata text-sm">Read Full Article</p>
+              <p className="text-stone-500 tracking-tighter font-inconsolata text-xs md:text-sm">Read Full Article</p>
             </div>
           </div>
         </div>
