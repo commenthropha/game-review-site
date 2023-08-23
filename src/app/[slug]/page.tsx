55efc6535db01f089e7f5b985e32cf5e1813a22f
Anees Haroon <aharoon1007@gmail.com>
2023-08-23 11:23:45 +0100

    Update /[slug]/page.tsx styling

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,6 @@
-import { getPosts } from "../(queries)";
+import { getPostBySlug, getPosts } from "../(queries)";
+import Image from "next/image";
+import moment from "moment";
 
 const generateStaticParams = async () => {
   const posts: Post[] = await getPosts();
@@ -8,8 +10,33 @@ const generateStaticParams = async () => {
   }));
 };
 
-const Review = ({ params }: { params: { slug: string } }) => {
-  return <div>My Post: {params.slug}</div>;
+const Review = async ({ params }: { params: { slug: string } }) => {
+  const post = await getPostBySlug(params.slug);
+
+  return (
+    <div className="w-4/5 md:w-3/4 xl:w-1/2 mx-auto">
+      <div className="mt-12 md:mt-24">
+        <h1 className="font-extrabold text-red-600 text-5xl md:text-6xl xl:text-7xl">
+          {post.title}
+        </h1>
+        <h2 className="mt-2 font-bold text-2xl md:text-3xl xl:text-4xl">
+          {post.subheading}
+        </h2>
+      </div>
+      <div className="mt-6 md:mt-10 relative h-[300px] xl:h-[500px] md:w-4/5 mx-auto">
+        <Image
+          alt={post.title}
+          src={post.featuredImage.url}
+          layout="fill" // Fill the parent dimensions
+          objectFit="cover" // Make the image fill the container
+        />
+      </div>
+      <div className="tracking-tighter font-inconsolata mt-2 flex flex-col md:flex-row justify-between md:w-4/5 mx-auto md:text-lg">
+        <p>{moment(post.createdAt).format("MMM DD, YYYY")}</p>
+        <p>Written by {post.author}</p>
+      </div>
+    </div>
+  );
 };
 
 export { generateStaticParams };
