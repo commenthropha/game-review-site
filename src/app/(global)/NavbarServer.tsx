c3f1cafb7f78f3d7d8d2ffc7711a15cc364005f6
Anees Haroon <aharoon1007@gmail.com>
2023-08-23 17:17:34 +0100

    Add comments to /(global)/NavbarServer.tsx

--- src/app/(global)/NavbarServer.tsx
+++ src/app/(global)/NavbarServer.tsx
@@ -1,6 +1,11 @@
 import getRecentPosts from "../(queries)/getRecentPosts";
 import NavbarClient from "./NavbarClient";
 
+/* 
+ * Need this component to serve the Navbar,
+ * since we require a parent component that
+ * is a server component to get query data
+ */
 const NavbarServer = async () => {
   const recentPosts: Post[] = await getRecentPosts();
 
