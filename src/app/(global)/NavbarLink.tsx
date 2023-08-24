7eb652ab02eb29e9e275f725409e0505844811c9
Anees Haroon <aharoon1007@gmail.com>
2023-08-24 10:17:30 +0100

    Format /(global)/NavbarLink.tsx file

--- src/app/(global)/NavbarLink.tsx
+++ src/app/(global)/NavbarLink.tsx
@@ -1,25 +1,22 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-/* 
+/*
  * Created a separate component for the Navbar
- * links to improve reusability and avoid repitition 
+ * links to improve reusability and avoid repitition
  */
 const NavbarLink = ({
   text, // The text to display for the link element
-  href // Link to redirect to when clicked
+  href, // Link to redirect to when clicked
 }: {
-  text: string, 
-  href: string
+  text: string;
+  href: string;
 }) => {
   return (
-    <Link
-    href= {href}
-    className="has-text-weight-semibold has-text-white"
-  >
-    {text}
-  </Link>
-  )
-}
+    <Link href={href} className="has-text-weight-semibold has-text-white">
+      {text}
+    </Link>
+  );
+};
 
-export default NavbarLink
+export default NavbarLink;
