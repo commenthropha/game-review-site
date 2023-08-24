1fd965e8f935ae92d60b3a7523815f88e2ffe9d9
Anees Haroon <aharoon1007@gmail.com>
2023-08-24 10:16:49 +0100

    Format /components/index.js file

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,4 +1,3 @@
 export { default as Hero } from "./Hero";
-export { default as About } from "./About"
-export { default as Reviews } from "./Reviews"
-
+export { default as About } from "./About";
+export { default as Reviews } from "./Reviews";
